0624bb1ab4db1eb1f24e7a7a10ba94887f6955ce
Muhammad Eesa Jan <mjanwarsi@gmail.com>
2018-10-08 12:26:30 +0500

    Added show page for campground

--- app.js
+++ app.js
@@ -1,84 +1,98 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const uuid = require("uuid");
 
 const campgrounds = [
   {
+    id: uuid(),
     name: "Virginia State Parks",
     url: "https://c1.staticflickr.com/3/2924/14465824873_71a21a6027_k.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Random",
     url: "https://c1.staticflickr.com/4/3741/9586943706_b22f00e403_b.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Random 2",
     url: "https://c1.staticflickr.com/8/7457/9586944536_9c61259490_c.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Diamond Moutain",
     url: "https://c1.staticflickr.com/1/76/216334882_067eb5f270_z.jpg?zz=1",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Joshua Tree National Park",
     url: "https://c1.staticflickr.com/6/5025/5620577903_24e2654dd8_b.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Mono Lake",
     url: "https://c1.staticflickr.com/7/6062/6065444573_9670dddaec_b.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Blue Tent Under Milkyway",
-    url: "images/blue-tent-under-milkyway.jpg",
+    url: "/images/blue-tent-under-milkyway.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Bonfire Surrounded With Green Grass Field",
-    url: "images/bonfire-surrounded-with-green-grass-field-1061640.jpg",
+    url: "/images/bonfire-surrounded-with-green-grass-field-1061640.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Green and White Tents Near Trees",
-    url: "images/green-and-white-tents-near-trees-939723.jpg",
+    url: "/images/green-and-white-tents-near-trees-939723.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Man and Woman Sitting beside Bonfire during Night time",
     url:
-      "images/man-and-woman-sitting-beside-bonfire-during-nigh-time-776117.jpg",
+      "/images/man-and-woman-sitting-beside-bonfire-during-nigh-time-776117.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Photosix Camping tents in Forest",
-    url: "images/photosix-camping-tents-in-forest-699558.jpg",
+    url: "/images/photosix-camping-tents-in-forest-699558.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Tent at the Field near Trees during Night",
-    url: "images/tent-at-the-field-near-trees-during-night.jpg",
+    url: "/images/tent-at-the-field-near-trees-during-night.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
+    id: uuid(),
     name: "Adventure Alps Camping",
-    url: "images/adventure-alps-camp-camping-618848.jpg",
+    url: "/images/adventure-alps-camp-camping-618848.jpg",
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
@@ -92,6 +106,12 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", (req, res) => {
   res.render("./campgrounds/campgrounds.ejs", { ejsCampgrounds: campgrounds });
+});
+app.get("/campgrounds/:campgroundId", (req, res) => {
+  const campground = campgrounds.find(
+    campground => campground.id === req.params.campgroundId
+  );
+  res.render("campgrounds/campground.ejs", { ejsCampground: campground });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
